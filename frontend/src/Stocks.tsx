8817d2e927e8c1002ce9556a5038f7d8152deacd
leo <jaykang1992@naver.com>
2023-09-16 15:52:17 +0900

    init

--- frontend/src/Stocks.tsx
+++ frontend/src/Stocks.tsx
@@ -1,6 +1,20 @@
-import React, { useState } from 'react';
+import React, {ButtonHTMLAttributes, FC, useState} from 'react';
 import axios from 'axios';
 import styles from './Stocks.module.css';
+
+
+interface PrettyButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const PrettyButton: FC<PrettyButtonProps> = ({ onClick, children }) => {
+    return (
+        <button className={styles.button} onClick={onClick}>
+            {children}
+        </button>
+    );
+};
+
 
 const Stocks: React.FC = () => {
     const [stocks, setStocks] = useState<string[]>([]);
@@ -9,6 +23,7 @@ const Stocks: React.FC = () => {
     const [fileName, setFileName] = useState<string>("");
     const [daysAgo, setDaysAgo] = useState<number>(2);
     const [channelRangeStocks, setChannelRangeStocks] = useState<string[]>([]);
+
 
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,25 +46,31 @@ const Stocks: React.FC = () => {
     };
 
     const handleUploadFile = async () => {
-        const selectedFile = document.querySelector<HTMLInputElement>('#fileInput')?.files?.[0];
-        if (!selectedFile) {
-            console.error("No file selected");
-            return;
-        }
+    // Clear previous stocks data
+    setStocks([]);
+    setJangtaStocks([]);
+    setChannelRangeStocks([]);
 
-        const formData = new FormData();
-        formData.append('file', selectedFile);
+    const selectedFile = document.querySelector<HTMLInputElement>('#fileInput')?.files?.[0];
+    if (!selectedFile) {
+        console.error("No file selected");
+        return;
+    }
 
-        try {
-            const response = await axios.post('http://localhost:8000/upload-csv/', formData);
-            console.log(response.data);
-            fetchJangtaData();  // Fetch the jangta stock data after uploading the file
-            fetchData();  // Fetch the danta stock data after uploading the file
-            fetchChannelRangeData();
-        } catch (error) {
-            console.error("Error uploading file:", error);
-        }
-    };
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+        const response = await axios.post('http://localhost:8000/upload-csv/', formData);
+        console.log(response.data);
+        fetchJangtaData();  // Fetch the jangta stock data after uploading the file
+        fetchData();  // Fetch the danta stock data after uploading the file
+        fetchChannelRangeData();
+    } catch (error) {
+        console.error("Error uploading file:", error);
+    }
+};
+
 
     const handleDaysAgoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setDaysAgo(Number(event.target.value));
@@ -79,22 +100,44 @@ const Stocks: React.FC = () => {
         }
     };
 
+        const computeDaysAgo = (selectedDate: Date) => {
+        const now = new Date();
+        const diffTime = Math.abs(now.getTime() - selectedDate.getTime());
+        return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    };
+
+    // Update your change handler
+    const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedDate = new Date(event.target.value);
+        const days = computeDaysAgo(selectedDate);
+        setDaysAgo(days);
+    };
+
     return (
     <div className={styles.container}>
-        <h2 className={styles.title}>Filtered Stocks</h2>
+            <h2 className={styles.title}>Filtered Stocks</h2>
 
-        <label>
-            Days Ago:
-            <input type="number" value={daysAgo} onChange={handleDaysAgoChange} />
-        </label>
-        <input type="file" id="fileInput" onChange={handleFileChange} />
-        <button onClick={handleUploadFile}>Upload CSV</button>
+            <label className={styles.label}>
+                Select Date:
+                <input
+                    type="date"
+                    className={styles.prettyInput}
+                    onChange={handleDateChange}
+                />
+            </label>
+            <br/>
+            <label className={styles.uploadLabel} htmlFor="fileInput">
+                Choose CSV
+            </label>
+            <input type="file" id="fileInput" onChange={handleFileChange} className={styles.hiddenInput} />
+
+            <PrettyButton onClick={handleUploadFile}>Upload CSV</PrettyButton>
         {loading && <p>Loading...</p>}
 
         <div className={styles.flexContainer}>
             {/* Jangta Stocks Column */}
             <div className={styles.tableContainer}>
-                <h3>Jangta Stocks</h3>
+                <h3>Jangta Stocks {jangtaStocks.length > 0 && <span>- Complete</span>}</h3>
                 <table className={styles.table}>
                     <thead>
                         <tr>
@@ -113,7 +156,7 @@ const Stocks: React.FC = () => {
 
             {/* Danta Stocks Column */}
             <div className={styles.tableContainer}>
-                <h3>Danta Stocks</h3>
+                <h3>Danta Stocks {stocks.length > 0 && <span>- Complete</span>}</h3>
                 <table className={styles.table}>
                     <thead>
                         <tr>
@@ -132,7 +175,7 @@ const Stocks: React.FC = () => {
 
             {/* Stocks Within ±5% of Lower Channel Column */}
             <div className={styles.tableContainer}>
-                <h3>Stocks Within ±7% of Lower Channel</h3>
+                <h3>Stocks Within ±7% of Lower Channel {channelRangeStocks.length > 0 && <span>- Complete</span>}</h3>
                 <table className={styles.table}>
                     <thead>
                         <tr>
